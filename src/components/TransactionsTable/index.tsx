9e605be43f85c49e4f721eae278a276c30fbd271
Luziane Freitas <freitas.lu@outlook.com>
2021-07-16 14:31:59 -0300

    add: api miragejs

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,15 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
 import { Container } from "./styles";
 
 export function TransactionsTable() {
+    useEffect(() => {
+        // requisição na api do tipo get
+        // request in get type api
+        api.get('transactions')
+            .then(response => console.log(response.data))
+    }, []);
+
     return (
        <Container>
            <table>
